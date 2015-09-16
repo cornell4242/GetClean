519978ddec4666a58c83e95cdeae8d008a2c6d37
Galina Patil <cornell4242@gmail.com>
2015-09-15 17:40:35 -0700

    Load files to Github, part 1

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# GetClean
-Getting and Cleaning Data Project
+---
+title: "[Getting and Cleaning Data](https://www.coursera.org/course/getdata) Project"
+output: html_document
+---
+## Overview
+The purpose of this project is to demonstrate the ability to collect, work with, and clean a data set.
+The data linked from the course website represent data collected from the accelerometers from the 
+Samsung Galaxy S smartphone. 
+
+Full description available from source: [UCI Machine Learning Repository](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)<p>
+
+[Source of data used for this analysis](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+
+### Contents of Github repository
+* __README.md__: General overview of the project and explanation of how script works, and creates the tidy data set
+* __CodeBook.md__: Detailed information about raw and tidy data sets, and details of the cleaning, as well as the creation of the tidy data set
+* __run_analysis.R__: R script Which performs the cleaning of the raw data, and creates the tidy data set.
+* __tidy.txt__: the resulting independent, tidy data set with the average of each variable for each activity and each subject
+
+## How the R script (run_analysis.R) works
+1.  Downloads, unzips and renames source files to working directory from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+2.  Loads key data tables and vectors:  measurement variable labels, activity key 
+3.  Loads raw test data (2947 observations) as 3 data tables: (1) subject tested, (2) activity performed, (3) data measurements 
+4.  Loads raw training data (7352 observations) as 3 data tables: (1) subject tested, (2) activity performed, (3) data measurements 
+5.  Combines test and training tables into 3 respective "total" data tables
+5.  Creates meaningful variable names for all variables
+6.  Cross references activity code to activity descriptions, and adds descriptive variable to total activity data table
+7.  Subsets total data measurement file to include only variables containing  mean ("mean") or standard deviation ("std") in variable descriptions - resulted in 86 variables
+8.  Creates "alldata" - a single, unified data frame (cbind) for all 10299 observations containing:  subject ID, Activity, Mean and Standard Deviation variables <u>(a total of 88 variables)</u>
+9.  Creates separate "tidy" data set from "alldata" as described in next section
+
+### General description of tidy data set creation
+
+1. Converts subject ID & activity description variables to variable type "factor"
+2. Using "alldata", creates "tidy" data set which finds the mean of each numeric variable, aggregating based on each unique Subject ID + Activity
+3. Writes the "tidy" data frame to a text file called <u>tidy.txt</u>, which is saved in the working directory  
---
+++ run_analysis.R
@@ -0,0 +1,97 @@
+# set the scene
+rm(list=ls())
+#library(dplyr)
+#library(reshape2)
+#download and unzip file into working directory and rename folder
+workingdir<-getwd()
+filetoload<- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(filetoload, destfile = "./UCI.zip")
+unzip("./UCI.zip")
+apropos("rename")
+file.rename("./UCI HAR Dataset", "./UCI")
+setwd("./UCI")
+
+# load (as character) general data & rename columns of activity 
+measurementLabels<- read.table("./features.txt")
+measurementLabels <- as.character(measurementLabels$V2)
+ 
+activity<- read.table("./activity_labels.txt") 
+names(activity)[1] <- "ActivityCode"
+names(activity)[2] <- "ActivityDesc"
+activity$ActivityDesc <- as.character(activity$ActivityDesc)
+
+#Load the test files
+setwd(paste(workingdir, "/UCI/test", sep = ""))
+testsubject <- read.table("./subject_test.txt")
+testx<- read.table("./x_test.txt")
+testlabels<- read.table("./y_test.txt")
+
+# Load the training pieces
+setwd(paste(workingdir, "/UCI/train", sep = ""))
+trainsubject <- read.table("./subject_train.txt")
+trainx<- read.table("./x_train.txt")
+trainlabels<- read.table("./y_train.txt")
+
+# combine test and train corresponding files, first test, then train
+totallabels<- rbind(testlabels, trainlabels)
+totalsubject<- rbind(testsubject, trainsubject)
+totalx<- rbind(testx, trainx)
+
+#cleanup files
+rm(testx, testlabels, testsubject, trainlabels, trainx, trainsubject)
+
+#rename variables
+names(totalsubject)[1] <- "SubjectID"
+names(totallabels)[1] <- "ActivityCode"
+names(totalx)<-measurementLabels
+
+# cross reference training labels to their activity descriptions and update
+nbig<- nrow(totallabels)
+nact<- nrow(activity)
+ActDesc<- character(length = nbig)
+
+totallabels<- cbind(totallabels, ActDesc)
+totallabels$ActDesc<- as.character(totallabels$ActDesc)
+
+#loop through and add description to each row of the code
+for(r in 1:nbig) {  #row of big table
+  for (a in 1:nact) {
+    if (totallabels[r,1] == activity[a,1]) {
+      totallabels[r,2] <- activity[a, 2]
+    }
+  }
+}
+
+# tidy up
+rm(a, ActDesc, nact, nbig, r)
+
+# 2 Extract only the measurements on the mean and std dev for each measurement
+
+mncol<- grep("[Mm]ean", measurementLabels)
+stdcol<- grep("[Ss]td", measurementLabels)
+mscol<- sort(c(mncol, stdcol))
+
+# keep only those columns of the data that are Averages or Means
+onlyms<- totalx[, mscol]
+
+# create unified data frame with all relevant data
+alldata<- cbind(onlyms, totalsubject, totallabels)
+alldata$ActivityCode<- NULL
+
+# 5 From the data set in step 4, create a second, 
+#   independent tidy data set with the average of each variable 
+#   for each activity and each subject
+
+#convert subject ID & activity descriptions to "factors"
+alldata$SubjectID<- as.factor(alldata$SubjectID)
+alldata$ActDesc<-as.factor(alldata$ActDesc)
+
+tidy <- aggregate(alldata, by=list(Activity = alldata$ActDesc, Subject = alldata$SubjectID), mean)
+
+#drop 2 meaningless columns
+tidy$SubjectID <- NULL
+tidy$ActDesc <- NULL
+
+# create a text file of the tidy data 
+setwd(workingdir)
+write.table(tidy, "tidy.txt", row.names = FALSE)
